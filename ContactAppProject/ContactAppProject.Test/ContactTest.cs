5eee344e850648aed8d3aa0e1e408e45b17e1ac5
Mateusz P <matprz11@gmail.com>
2025-03-04 13:02:50 +0100

    Added tests for Contact

---
+++ ContactAppProject/ContactAppProject.Test/ContactTest.cs
@@ -0,0 +1,43 @@
+﻿using ContactAppProject.models;
+
+namespace ContactAppProject.Test;
+
+public class ContactTest
+{
+    [Fact]
+    public void Constructor_SetsAllPropertiesCorrectly()
+    {
+        //Arrange
+        string firstName = "John";
+        string lastName = "Doe";
+        string email = "john@doe.com";
+        string phone = "0888888888";
+        
+        //Act
+        Contact contact = new Contact(firstName, lastName, email, phone);
+        
+        //Assert
+        Assert.Equal(firstName, contact.FirstName);
+        Assert.Equal(lastName, contact.LastName);
+        Assert.Equal(email, contact.Email);
+        Assert.Equal(phone, contact.PhoneNumber);
+    }
+
+    [Fact]
+    public void ToString_ReturnsCorrectString()
+    {
+        //Arrange
+        string firstName = "John";
+        string lastName = "Doe";
+        string email = "john@doe.com";
+        string phone = "0888888888";
+        Contact contact = new Contact(firstName, lastName, email, phone);
+        
+        //Act
+        var result = contact.ToString();
+        
+        //Assert
+        Assert.Equal($"Name: {firstName} {lastName}, Email: {email}, Phone: {phone}", result);
+        
+    }
+}
